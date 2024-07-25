1c4e933002f9e688742716622a64597e7b730ed8
AHAMAD RAJA <ahamad.20405@knit.ac.in>
2024-07-25 15:21:52 +0530

    Your commit message here

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const socketIO = require("socket.io");
 
 const app=express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 const users=[{}];
 app.use(cors());
